52784864947ae68c8e2f38d50c9d10fcb5a68ff9
Cordinal-Kid <harvest9231@gmail.com>
2026-06-22 17:03:30 +0800

    Site updated: 2026-06-22 17:03:26

--- libs/codeBlock/codeShrink.js
+++ libs/codeBlock/codeShrink.js
@@ -1,16 +1,29 @@
 // 代码块收缩
 
 $(function () {
-    var $code_expand = $('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');
+    var collapseThreshold = 360;
 
-    $('.code-area').prepend($code_expand);
+    $('.code-area').each(function () {
+      var $wrap = $(this);
+      var pre = $wrap.children('pre')[0];
 
-    $('.code-expand').on('click', function () {
-      var $wrap = $(this).closest('.code-area');
-      $wrap.toggleClass('code-closed');
-      $(this).toggleClass('fa-angle-up fa-angle-down');
+      if (!pre || pre.scrollHeight <= collapseThreshold) {
+        return;
+      }
+
+      var $toggle = $('<button type="button" class="code-expand" title="展开代码" aria-label="展开代码" aria-expanded="false"><i class="fas fa-angle-down" aria-hidden="true"></i></button>');
+      $wrap.addClass('code-collapsible code-closed').prepend($toggle);
+
+      $toggle.on('click', function () {
+        var isClosed = $wrap.toggleClass('code-closed').hasClass('code-closed');
+        $(this)
+          .attr('aria-expanded', String(!isClosed))
+          .attr('aria-label', isClosed ? '展开代码' : '收起代码')
+          .attr('title', isClosed ? '展开代码' : '收起代码')
+          .find('i')
+          .toggleClass('fa-angle-down', isClosed)
+          .toggleClass('fa-angle-up', !isClosed);
+      });
     });
-
-
 });
 
